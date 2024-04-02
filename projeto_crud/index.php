b7a3e36916d73f809dc27ea472a10030f5dc4994
Jeandra <jeandra8789@gmail.com>
2024-04-02 20:43:36 -0300

    Create index.php

---
+++ projeto_crud/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Aplicação CRUD</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>Bem-vindo à Minha Aplicação CRUD</h1>
+    <p>Esta é uma aplicação simples para gerenciar informações de alunos.</p>
+    
+    <h2>O que você gostaria de fazer?</h2>
+    <ul>
+        <li><a href="create.php">Adicionar Novo Aluno</a></li>
+        <li><a href="read.php">Visualizar Alunos</a></li>
+        <li><a href="update.php">Atualizar Dados do Aluno</a></li>
+        <!--<li><a href="delete.php">Excluir Aluno</a></li>-->
+    </ul>
+    
+    <p>Se precisar de ajuda, <a href="#">entre em contato conosco</a>.</p>
+</div>
+
+</body>
+</html>
