3797c23d660e09d94bad167b71e2ccba0fcba950
Jeandra <jeandra8789@gmail.com>
2024-04-03 18:09:21 -0300

    Update index.php

--- projeto_crud/index.php
+++ projeto_crud/index.php
@@ -7,17 +7,17 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-
+<?php include 'dark.php'; ?>
 <div class="container">
-    <h1>Bem-vindo à Minha Aplicação CRUD</h1>
+    <h2>Bem-vindo à Minha Aplicação CRUD</h2>
     <p>Esta é uma aplicação simples para gerenciar informações de alunos.</p>
     
-    <h2>O que você gostaria de fazer?</h2>
+    <h3>O que você gostaria de fazer?</h3>
     <ul>
         <li><a href="create.php">Adicionar Novo Aluno</a></li>
         <li><a href="read.php">Visualizar Alunos</a></li>
-        <li><a href="update.php">Atualizar Dados do Aluno</a></li>
-        <!--<li><a href="delete.php">Excluir Aluno</a></li>-->
+        <!--<li><a href="update.php">Atualizar Dados do Aluno</a></li>
+        <li><a href="delete.php">Excluir Aluno</a></li>-->
     </ul>
     
     <p>Se precisar de ajuda, <a href="#">entre em contato conosco</a>.</p>
